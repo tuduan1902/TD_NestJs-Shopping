d9d4876683d356bcf253f1c502ef78e83e54bc96
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-24 00:40:56 +0700

    Import mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://tuduan1902:PSDjTtAsSMGkWOtD@cluster0.lyxc4.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
